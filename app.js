8b0036df17829ca83446757cd7341f3fc318af15
Lika <lika.rossinskaya@gmal.com>
2022-10-25 17:19:20 +0300

    template

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const fs = require("fs/promises");
+const nanoid = require("nanoid");
+const path = require("path");
+const multer = require("multer");
+
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.set("json space", 8);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
