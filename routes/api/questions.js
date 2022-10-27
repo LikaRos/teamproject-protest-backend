8c24aa3eca80aa79076778373a00bf2bc0e71e12
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-10-28 02:10:38 +0300

    Update questions.js

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -7,13 +7,10 @@ const { schemas } = require("../../models/question");
 const router = express.Router();
 
 router.get("/:type/random", ctrlWrapper(ctrl.getRandomQuestions));
-<<<<<<< Updated upstream
+
 router.get("/result", ctrlWrapper(ctrl.getResults));
 // localhost:3001/api/questions/tech/random
-=======
-router.get("/result", ctrlWrapper(ctrl.getRandomQuestions));
-//localhost:3001/api/questions/tech/random
->>>>>>> Stashed changes
+
 // localhost:3001/api/questions/theory/random;
 
 module.exports = router;
