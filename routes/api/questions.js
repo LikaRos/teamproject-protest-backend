11d8a798c0f51e7274d6fe9e2466fcc5f7eda532
Lika <lika.rossinskaya@gmal.com>
2022-10-27 23:35:32 +0300

    server changed

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -7,7 +7,7 @@ const { schemas } = require("../../models/question");
 const router = express.Router();
 
 router.get("/:type/random", ctrlWrapper(ctrl.getRandomQuestions));
-//localhost:3000/api/questions/tech/random
-// localhost:3000/api/questions/theory/random;
+//localhost:3001/api/questions/tech/random
+// localhost:3001/api/questions/theory/random;
 
 http: module.exports = router;
