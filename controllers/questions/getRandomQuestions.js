5648e8f3dace8d3a5931d11eaa5d4dd01d8d2cb6
Lika <lika.rossinskaya@gmal.com>
2022-10-28 12:48:24 +0300

    dymanic type request

--- controllers/questions/getRandomQuestions.js
+++ controllers/questions/getRandomQuestions.js
@@ -1,10 +1,12 @@
 const { Question } = require("../../models/question");
 
 const getRandomQuestions = async (req, res) => {
+  const { type } = req.params;
   const result = await Question.aggregate([
-    { $match: { type: "tech" } },
+    { $match: { type: `${type}` } },
     { $sample: { size: 12 } },
   ]);
+  console.log(req.params);
 
   res.status(201).json(result);
 };
