6a86cc67d4d29d20a3b36f465fcc9f5b9891f7df
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-10-31 11:36:18 +0200

    fix auth

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -10,7 +10,6 @@ const { RequestError, createVerifyEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
-
   const user = await User.findOne({ email });
 
   if (user) {
