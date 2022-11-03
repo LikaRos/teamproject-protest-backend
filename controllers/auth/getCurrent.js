909b25ed783d05e63805bcf146c63bcbfd450023
andrii_h <galgayand@gmail.com>
2022-11-03 16:08:14 +0200

    Update getCurrent.js

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,9 +1,17 @@
-const getCurrent = async (req, res) => {
-  const { email } = req.user;
+const { User } = require('../../models/user');
+const { RequestError } = require('../../helpers');
 
-  res.json({
-    email
+const getCurrentUser = async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new RequestError(401, 'Not authorized');
+  }
+  const { email, verify } = user;
+
+  res.status(200).json({
+    email,
+    verify,
   });
 };
 
-module.exports = getCurrent;
+module.exports = getCurrentUser;
