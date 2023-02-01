b9cd43edaa602660819a23e798f46924e93cb839
Lika <lika.rossinskaya@gmail.com>
2023-02-02 00:43:45 +0200

    port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST, PORT = 3001 } = process.env;
+// const { DB_HOST, PORT = 3001 } = process.env;
+
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
